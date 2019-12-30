79306e5b14bf881a4f1b8e6835ef72c6e3425601
Thales Monteiro <thales.monteiro@eng.ci.ufpb.br>
2019-12-29 23:37:23 -0300

    Webpack atualizado

---
+++ ES6/desafio/src/functions.js
@@ -0,0 +1,8 @@
+export const idade = 22;
+
+export class Usuario{
+    static info(){
+        console.log('Apenas um teste');
+    }
+
+}
---
+++ ES6/desafio/src/main.js
@@ -0,0 +1,4 @@
+import {Usuario as classeUsuario, idade as idadeUsuario } from './functions'
+
+classeUsuario.info();
+console.log(idadeUsuario);
